5783909130a7c9e1f497993c41a74cd404c524ca
Echo <github@3kh0.net>
2025-05-22 18:54:34 -0400

    add aliases for subcommands

--- app.js
+++ app.js
@@ -22,10 +22,10 @@ app.command("/stacked", async ({ command, ack, respond }) => {
   if (["inventory", "inv", "i", "in"].includes(subcommand)) route = "inventory";
   else if (["unbox", "open", "u", "ub", "unb", "hourly", "un", "h"].includes(subcommand)) route = "unbox";
   else if (["sell", "s", "sl"].includes(subcommand)) route = "sell";
+  else if (["buy", "b", "bu", "purchase"].includes(subcommand)) route = "buy";
   else if (["welcome", "start", "onboard"].includes(subcommand)) route = "welcome";
-  else if (["inspect", "item", "info", "stats"].includes(subcommand)) route = "item";
+  else if (["inspect", "ins", "item", "info", "stats"].includes(subcommand)) route = "item";
   else if (["use", "a", "attack", "heal", "hit"].includes(subcommand)) route = "use";
-  else if (subcommand === "buy") route = "buy";
   else if (["opt", "opt-in", "optin", "opt-out", "optout"].includes(subcommand)) route = "opt";
   else if (["help", "h", "?", "commands", "cmds"].includes(subcommand)) route = "help";
 
@@ -40,7 +40,7 @@ app.command("/stacked", async ({ command, ack, respond }) => {
       client: app.client,
     }); // <-- add client
   } else {
-    await respond("failed to find route to a valid command");
+    await respond(":red-x: No sub-command found! If you are lost, try `/stacked help` for a list of commands.");
   }
 });
 
