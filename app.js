5fa4bb83dc53ff4de5590c4a0d5e7c9ddfed08fd
Echo <github@3kh0.net>
2025-05-23 13:49:04 +0000

    reorder app.js to start slack then healthcheck

--- app.js
+++ app.js
@@ -1,11 +1,50 @@
 const { App } = require("@slack/bolt");
-const fs = require("fs");
-const path = require("path");
 
 require("dotenv").config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+app.command("/stacked", async ({ command, ack, respond }) => {
+  await ack();
+  const args = command.text.trim().split(/\s+/);
+  const subcommand = args[0]?.toLowerCase();
+  if (!subcommand) {
+    await respond(":red-x: No sub-command provided! If you are lost, try `/stacked help` for a list of commands.");
+    return;
+  }
+  let route = subcommand;
+
+  // define real routes, TODO add aliases
+  if (["inventory", "inv", "i", "in"].includes(subcommand)) route = "inventory";
+  else if (["unbox", "open", "u", "ub", "unb", "hourly", "un", "h"].includes(subcommand)) route = "unbox";
+  else if (["sell", "s", "sl"].includes(subcommand)) route = "sell";
+  else if (["buy", "b", "bu", "purchase"].includes(subcommand)) route = "buy";
+  else if (["welcome", "start", "onboard"].includes(subcommand)) route = "welcome";
+  else if (["inspect", "ins", "item", "info", "stats"].includes(subcommand)) route = "item";
+  else if (["use", "a", "attack", "heal", "hit"].includes(subcommand)) route = "use";
+  else if (["opt", "opt-in", "optin", "opt-out", "optout"].includes(subcommand)) route = "opt";
+  else if (["help", "h", "?", "commands", "cmds"].includes(subcommand)) route = "help";
+  else if (["daily", "d", "day"].includes(subcommand)) route = "daily";
+  else if (["weekly", "w", "week"].includes(subcommand)) route = "weekly";
+  else if (["coinflip", "cf", "flip", "coin", "cflip", "gamba", "bet"].includes(subcommand)) route = "coinflip";
+
+  // last ditch effort to match the command
+  const fs = require("fs");
+  const path = require("path");
+  const commandPath = path.join(__dirname, "commands", `${route}.js`);
+  if (fs.existsSync(commandPath)) {
+    const handler = require(commandPath);
+    await handler({
+      args: args.slice(1),
+      respond,
+      command,
+      client: app.client,
+    }); // <-- add client
+  } else {
+    await respond(":red-x: No sub-command found! If you are lost, try `/stacked help` for a list of commands.");
+  }
 });
 
 const http = require("http");
@@ -26,45 +65,6 @@ http
     }
   })
   .listen(3001);
-
-app.command("/stacked", async ({ command, ack, respond }) => {
-  await ack();
-  const args = command.text.trim().split(/\s+/);
-  const subcommand = args[0]?.toLowerCase();
-  if (!subcommand) {
-    await respond(":red-x: No sub-command provided!");
-    return;
-  }
-  let route = subcommand;
-
-  // define real routes, TODO add aliases
-  if (["inventory", "inv", "i", "in"].includes(subcommand)) route = "inventory";
-  else if (["unbox", "open", "u", "ub", "unb", "hourly", "un", "h"].includes(subcommand)) route = "unbox";
-  else if (["sell", "s", "sl"].includes(subcommand)) route = "sell";
-  else if (["buy", "b", "bu", "purchase"].includes(subcommand)) route = "buy";
-  else if (["welcome", "start", "onboard"].includes(subcommand)) route = "welcome";
-  else if (["inspect", "ins", "item", "info", "stats"].includes(subcommand)) route = "item";
-  else if (["use", "a", "attack", "heal", "hit"].includes(subcommand)) route = "use";
-  else if (["opt", "opt-in", "optin", "opt-out", "optout"].includes(subcommand)) route = "opt";
-  else if (["help", "h", "?", "commands", "cmds"].includes(subcommand)) route = "help";
-  else if (["daily", "d", "day"].includes(subcommand)) route = "daily";
-  else if (["weekly", "w", "week"].includes(subcommand)) route = "weekly";
-  else if (["coinflip", "cf", "flip", "coin", "cflip", "gamba", "bet"].includes(subcommand)) route = "coinflip";
-
-  // last ditch effort to match the command
-  const commandPath = path.join(__dirname, "commands", `${route}.js`);
-  if (fs.existsSync(commandPath)) {
-    const handler = require(commandPath);
-    await handler({
-      args: args.slice(1),
-      respond,
-      command,
-      client: app.client,
-    }); // <-- add client
-  } else {
-    await respond(":red-x: No sub-command found! If you are lost, try `/stacked help` for a list of commands.");
-  }
-});
 
 (async () => {
   await app.start(process.env.PORT || 3000);
