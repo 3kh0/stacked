0cde6f7999a7aa5187a0cedb1f7e5c892961921c
Echo <github@3kh0.net>
2025-05-26 11:29:19 -0400

    remove allowlist :eyes:

--- commands/welcome.js
+++ commands/welcome.js
@@ -12,37 +12,6 @@ module.exports = async ({ respond, command }) => {
     return;
   }
 
-  const allowlist = [
-    "U080A3QP42C",
-    "U08TBE25U82",
-    "U066LL4JBDL",
-    "U062U3SQ2T1",
-    "U088DPHNEF8",
-    "U082GTRTR5X",
-    "U08290982KU",
-    "U078H10Q3MZ",
-    "U06FG6G6SNL",
-    "U07F3DZ7PU2",
-    "U07SX29CECA",
-    "U08F7A2HBPZ",
-    "U07V3MD8BPW",
-    "U06JLP2R8JV",
-    "U07BU2HS17Z",
-    "U07UV4R2G4T",
-    "U07PYPAF5RR",
-    "U071JHBEJ7R",
-    "U082RFNDVKQ",
-    "U04KNK837S4",
-    "U07L45W79E1",
-    "U0C7B14Q3",
-  ];
-  if (!allowlist.includes(slack_uid)) {
-    await respond(
-      ":red-x: You have yet to be whitelisted. Please contact <@U080A3QP42C> if you think this is a mistake.",
-    );
-    return;
-  }
-
   if (!user && (!error || error.code === "PGRST116")) {
     const { error: insErr } = await supabase.from(usersTable).insert({
       slack_uid,
